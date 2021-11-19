ba34d4edc0f9cb175d2bb337bc36f378090ac112
NikolaiKruchkov <47074017+NikolaiKruchkov@users.noreply.github.com>
2021-11-19 17:51:51 +0300

    Add files via upload

---
+++ Zadanie massiv 2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Zadanie_massiv_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целочисленное значение N затем нажмите Enter");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] massiv = new int[n, n];
+            
+            for (int i = 0; i < n; i+=2)
+            {
+                
+                for (int j = 0; j < n; j+=2)
+                {
+                    massiv[i, j] = 1;                       
+                               
+                 }
+                for (int j = 1; j < n; j += 2)
+                {
+                    massiv[i, j] = 0;
+
+                }
+
+            }
+            for (int i = 1; i < n; i += 2)
+            {
+
+                for (int j = 0; j < n; j += 2)
+                {
+                    massiv[i, j] = 0;
+
+                }
+                for (int j = 1; j < n; j += 2)
+                {
+                    massiv[i, j] = 1;
+
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("{0}", massiv[i, j]);
+
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.Write("Спасибо, что использовали нашу программу, для выхода нажмите любую клавишу");
+            Console.ReadKey();
+
+
+
+        }
+
+
+    }
+
+}
---
+++ Zadanie massiv/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Zadanie_massiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] massiv = new double[7];
+            int k = 1;
+            double sum = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine("Введите значение {0} элемента массива затем нажмите Enter", k);
+                double n = Convert.ToDouble(Console.ReadLine());
+                massiv[i] = n;
+                k += 1;
+                sum = sum + n;
+            }
+
+            Console.WriteLine("Среднее арифмитическое элементов массива равно {0, 4:f2}", sum / 7);
+            Console.Write("Спасибо, что использовали нашу программу, для выхода нажмите любую клавишу");
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
